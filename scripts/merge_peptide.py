38676d01b4808469f531465213bc5697a832280c
patruong <ptruong@kth.se>
2020-10-16 11:42:24 +0200

    add

--- scripts/merge_peptide.py
+++ scripts/merge_peptide.py
@@ -12,31 +12,33 @@ import time
 import pandas as pd
 import numpy as np
 
-file_prefix = "peptide_tryptic_logFC2_i"
-
-files = sorted(os.listdir())
-res = pd.DataFrame()
-start_time = time.time()
-tmp_time = start_time
-n_files = int(len(files)/2)
-for i in range(180):
-    filename = files[i]
-    print("Reading in " + filename + " : "+ str(i) + "/" + str(len(files)))
-    df = pd.read_csv(filename, sep = "\t")
-    df_PEP_treshold = df[df["PEP"] < 0.01]
-    df_PEP_MissedCleaveges_treshold = df_PEP_treshold[df_PEP_treshold["Missed_Cleaveges"] < 2]
-    #del df_PEP_treshold
-    #df = df_PEP_MissedCleaveges_treshold
-    #del df_PEP_MissedCleaveges_treshold
-    #df = df.drop(["Missed_Cleaveges", "Charges", "Score", "PEP", "Gene_names"], axis = 1)
-    res = res.append(df)
-    proc_time = time.time()
-    print("From start:" + str(proc_time - start_time))
-    print("Iter time:" + str(proc_time - tmp_time))
-    tmp_time = proc_time
+def merge_peptide_dfs():
+#    file_prefix = "peptide_tryptic_logFC2_i"
     
-print("Process finished: " + str(time.time() - start_time))
-
-
-
-
+    files = sorted(os.listdir())
+    res = pd.DataFrame()
+    start_time = time.time()
+    tmp_time = start_time
+    #n_files = int(len(files)/2)
+    for i in range(180):
+        filename = files[i]
+        print("Reading in " + filename + " : "+ str(i) + "/" + str(len(files)))
+        df = pd.read_csv(filename, sep = "\t")
+        df_PEP_treshold = df[df["PEP"] < 0.01] # PEP treshold
+        df_PEP_MissedCleaveges_treshold = df_PEP_treshold[df_PEP_treshold["Missed_Cleaveges"] < 2] #missed lceaves treshold
+        del df_PEP_treshold
+        df = df_PEP_MissedCleaveges_treshold
+        del df_PEP_MissedCleaveges_treshold
+        df = df.drop(["Missed_Cleaveges", "Charges", "Score", "PEP", "Gene_names"], axis = 1) # Drop unnessasary columns
+        res = res.append(df)
+        proc_time = time.time()
+        print("From start:" + str(proc_time - start_time))
+        print("Iter time:" + str(proc_time - tmp_time))
+        tmp_time = proc_time
+        
+    print("Process finished: " + str(time.time() - start_time))
+    res.to_csv("peptide_melted.csv", sep = "\t", index = False)
+    print("DONE!")
+    
+    
+    
