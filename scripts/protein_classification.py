a85c9bdc7b3fae0890c7b4f48eca84d42f966ad9
patruong <ptruong@kth.se>
2020-10-18 00:22:47 +0200

    protein_class

--- scripts/protein_classification.py
+++ scripts/protein_classification.py
@@ -8,14 +8,16 @@ Created on Sat Oct 17 21:02:29 2020
 
 import pandas as pd
 import numpy as np
+from multiprocessing import  Pool
+
 
 from uniprot import *
 
-df = pd.read_csv("melted_treshold.csv", sep ="\t")
+df = pd.read_csv("../data/melted_treshold.csv", sep ="\t")
 
 df.Leading_razor_protein[0]
 #df.Proteins[0]
-df_sub = df[0:200]
+df_sub = df[0:1000]
 
 import time
 
@@ -38,7 +40,20 @@ print(end_time - start_time)
 
 
 
+def parallelize_dataframe(df, func, n_cores=4):
+    df_split = np.array_split(df, n_cores)
+    pool = Pool(n_cores)
+    df = pd.concat(pool.map(func, df_split))
+    pool.close()
+    pool.join()
+    return df
+start_time=time.time()
+df_go = parallelize_dataframe(df_sub.Leading_razor_protein, go_class)
+end_time=time.time()
+print(end_time - start_time)
 
 
-
-
+start_time=time.time()
+var = pool.map(go_class, df_sub.Leading_razor_protein)
+end_time=time.time()
+print(end_time - start_time)
