ee9a8b8e49e6bd6db6166c01c6396ab0849ded3a
patruong <ptruong@kth.se>
2021-03-24 16:44:57 +0100

    upd

--- scripts/working_script_peptide_tryptic_20210324.py
+++ scripts/working_script_peptide_tryptic_20210324.py
@@ -31,37 +31,10 @@ df_raw = pd.read_csv("peptides tryptic.csv", sep = "\t")
 cell_lines, states, replicates = get_cell_line_state_replicate()
 base_cols = get_base_cols_peptide()
 reporter_intensity_corrected_cols = get_all_reporter_intensity_correct()
-df_base = df[get_base_cols_peptide()]
+df_base = df_raw[get_base_cols_peptide()]
 
 
-
-def intensities_to_midx_df(df_int):
-    """
-    df_int = df[reporter_intensity_corrected_cols]
-
-    example on selecting from levels on multiIndex:
-        df_int.iloc[:, df_int.columns.get_level_values(level=1) == "S"]
-        
-    Example used for this build.
-    
-    Column to midx
-    https://riptutorial.com/pandas/example/18696/how-to-change-standard-columns-to-multiindex
-    
-    Select columns in multiIdx
-    https://stackoverflow.com/questions/25189575/pandas-dataframe-select-columns-in-multiindex
-    
-    # build hierarchical df
-    
-    df_ = pd.DataFrame(np.random.randn(2,3), columns=['a','b','c'])
-    midx = pd.MultiIndex(levels=[['zero', 'one'], ['x','y']], codes=[[1,1,0,],[1,0,1,]])
-    df_.columns = midx
-    
-    
-    list1 = ['a','b','c','d'] # values
-    list2 = ['d','a', "c", "d"] # value list
-    print([list2.index(x) for x in list1 if x in list2])
-
-    """
+def col_to_mIdx(df_int):
     treatment_mapper = lambda x : x.split(" ")[3]
     specie_mapper = lambda x : x.split(" ")[4].split("_")[0]
     state_mapper = lambda x : x.split(" ")[4].split("_")[1]
@@ -93,9 +66,42 @@ def intensities_to_midx_df(df_int):
     
     midx = pd.MultiIndex(levels=[specie_list, state_list, treatment_list, replicate_list], 
                          codes=[specie_code, state_code, treatment_code, replicate_code])
+    return midx
+
+def intensities_to_midx_df(df_int):
+    """
+    df_int = df[reporter_intensity_corrected_cols]
+
+    example on selecting from levels on multiIndex:
+        df_int.iloc[:, df_int.columns.get_level_values(level=1) == "S"]
+        
+    Example used for this build.
+    
+    Column to midx
+    https://riptutorial.com/pandas/example/18696/how-to-change-standard-columns-to-multiindex
+    
+    Select columns in multiIdx
+    https://stackoverflow.com/questions/25189575/pandas-dataframe-select-columns-in-multiindex
+    
+    # build hierarchical df
+    
+    df_ = pd.DataFrame(np.random.randn(2,3), columns=['a','b','c'])
+    midx = pd.MultiIndex(levels=[['zero', 'one'], ['x','y']], codes=[[1,1,0,],[1,0,1,]])
+    df_.columns = midx
+    
+    
+    list1 = ['a','b','c','d'] # values
+    list2 = ['d','a', "c", "d"] # value list
+    print([list2.index(x) for x in list1 if x in list2])
+
+    """
+    midx = col_to_mIdx(df_int)
     df_int.columns = midx
     return df_int
 
+def drop_zero_row(df):
+    return df[(df.T != 0).any()]
+    
 # filter df
 df_base.columns
 df = df_raw[df_raw.PEP < 0.05] # 5% PEP removed 9578 peptides
@@ -103,11 +109,97 @@ df = df[df["Missed cleavages"] < 3] # 0 removed
 
 df_int = df[reporter_intensity_corrected_cols]
 df_int = df_int.drop_duplicates() # Removing duplicate rows, these are most likely 0 rows. 2126 rows dropped
+df_int = drop_zero_row(df_int) # dropped last zero-row
+midx = col_to_mIdx(df_int)
+
+# Raw
 df_int = intensities_to_midx_df(df_int)
 
-df_int.T
+# log2
+df_int = df_int.replace(0, np.nan)
+df_int = np.log2(df_int)
 
 
+# Aitchison
+#df_int = closure(df_int)
+#df_int = pd.DataFrame(df_int, columns = midx)
+
+# Aitchison multiplicative_replacement
+#df_int = multiplicative_replacement(df_int)
+#df_int = pd.DataFrame(df_int, columns = midx)
 
 
+########
+# PLOT #
+########
+
+def plot_intensity_histogram(df_int, min_x = 0, max_x = 30, step = 0.1):
+    """
+    df_int with midx
+    
+    plots the step histogram for {cell_line}_{state}
+    """
+    colors = ['b', 'g', 'r', 'c', 'm', 'y']
+    col_i = 0
+    for cell_line in cell_lines:
+        for state in states:
+            df_state = df_int.iloc[:, df_int.columns.get_level_values(level=1) == state]
+            df_cell_state = df_state.iloc[:, df_state.columns.get_level_values(level=0) == cell_line]
+            plot_label = True
+            for i in range(np.shape(df_cell_state)[1]):
+                if plot_label == True:
+                    plt.hist(df_cell_state.iloc[:,i] , bins=np.arange(min_x,max_x, step), 
+                             histtype="step", color = colors[col_i], alpha = 0.4,
+                             label = cell_line + "_" + state)
+                    plot_label = False
+                else:
+                    plt.hist(df_cell_state.iloc[:,i] , bins=np.arange(min_x,max_x, step), 
+                             histtype="step", color = colors[col_i], alpha = 0.4)
+            col_i+=1
+    plt.legend()
+
+plot_intensity_histogram(df_int, min_x = 0, max_x = 25, step = 0.1)
+
+def plot_intensity_boxplot(df_int):
+    """
+    Boxplot details.
+    # https://stackoverflow.com/questions/41997493/python-matplotlib-boxplot-color
+
+    df_int with midx
+    
+    plots the step histogram for {cell_line}_{state}
+    """
+    colors = ['b', 'g', 'r', 'c', 'm', 'y']
+    col_i = 0
+    pos_i = 0
+    for cell_line in cell_lines:
+        for state in states:
+            df_state = df_int.iloc[:, df_int.columns.get_level_values(level=1) == state]
+            df_cell_state = df_state.iloc[:, df_state.columns.get_level_values(level=0) == cell_line]
+            plot_label = True
+            for i in range(np.shape(df_cell_state)[1]):
+                if plot_label == True:
+                    plt.boxplot(df_cell_state.iloc[:,i].dropna(), positions = [pos_i], patch_artist=True, 
+                                notch=True, medianprops=dict(color="grey"),
+                                boxprops=dict(facecolor = colors[col_i]),)
+                                # No label yet, so if-else does not matter.......
+                    plot_label = False
+                else:
+                    plt.boxplot(df_cell_state.iloc[:,i].dropna(), positions = [pos_i], patch_artist=True, 
+                                notch=True, medianprops=dict(color="grey"),
+                                boxprops=dict(facecolor = colors[col_i]))
+                pos_i+=1
+            col_i+=1
+    plt.legend()
+
+plot_intensity_boxplot(df_int)
+
+plt.boxplot(df_cell_state.iloc[:,i].dropna(), positions = [1], patch_artist=True, 
+            notch=True, medianprops=dict(color="grey"),
+            boxprops=dict(facecolor = "red"))
+
+
+plt.hist(df_int.iloc[:,1] , bins=np.arange(min_x,max_x, step), 
+         histtype="step", color = colors[col_i], alpha = 0.4)
+plt.boxplot(df_int.iloc[:,1].dropna(), positions = [0], patch_artist=True, notch=True, medianprops=dict(color="grey"),boxprops=dict(facecolor = "red"))
 
